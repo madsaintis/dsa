5be1c2f9af5a56f3c9bfe2290f54f7dd83a18707
madsaintis <muhammadafifazhan@graduate.utm.my>
2023-11-09 13:55:57 +0800

    Adjusted code structure using function hoisting

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -1,21 +1,10 @@
-// Function to display array content
-const displayArrayElements = (arr) => {
-    let string = "";
-
-    for(const nums of arr)
-        string += nums + ', '
-
-    string = string.trim();
-    string = string.slice(0, string.length - 1)
-    console.log(string + '\n')
-}
-
 // Selection Sort - Time complexity O(n^2), Space Complexity O(1)
 
 // Selection sort is a comparison sorting algorithm that 
 // repeatedly selects the smallest (or largest, depending on sorting order) 
 // element from the unsorted section and 
 // moves it to the beginning (or end).
+
 
 // Selection Sort for sorting in descending order
 const selectionSort = (arr) => {
@@ -42,9 +31,23 @@ const selectionSort = (arr) => {
     displayArrayElements(arr);
 }
 
-
 let array = [1,2,3,4,5,6,7,8];
 let array2 = [11, 25, 12, 22, 64];
 
 selectionSort(array);
 selectionSort(array2);
+
+
+// Function to display array content
+function displayArrayElements (arr) {
+    let string = "";
+
+    for(const nums of arr)
+        string += nums + ', '
+
+    string = string.trim();
+    string = string.slice(0, string.length - 1)
+    console.log(string + '\n')
+}
+
+
